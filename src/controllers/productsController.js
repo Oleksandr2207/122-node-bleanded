55f97ba9b548f15630d83e4d24807d2338411499
Oleksandr Ponomarenko <osponomarenko09@gmail.com>
2026-03-22 16:00:20 +0200

    Merge pull request #3 from Oleksandr2207/task-4
    task4

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -15,4 +15,9 @@ export const getProductById = async (req, res) => {
   }
 
   res.status(200).json(product);
-}
+};
+
+export const createProduct = async (req, res) => {
+  const product = await Product.create(req.body);
+  res.status(201).json(product);
+};
