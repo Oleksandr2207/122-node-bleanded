d365b962eb3df96361053046c5b3043b79904c05
Uffaser <gerasimenkom405@gmail.com>
2026-04-05 15:16:20 +0300

    task-03

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,6 +2,7 @@ import createHttpError from 'http-errors';
 import { User } from '../db/models/User.js';
 import bcrypt from 'bcrypt';
 import { createCookies, createSession } from '../services/auth.js';
+import { Session } from '../db/models/Session.js';
 
 export const registerUser = async (req, res) => {
   const { email, password } = req.body;
@@ -20,4 +21,25 @@ export const registerUser = async (req, res) => {
   createCookies(res, session);
 
   res.status(201).json(newUser);
+};
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw createHttpError(401);
+  }
+
+  const isValidPassword = await bcrypt.compare(password, user.password);
+
+  if (!isValidPassword) {
+    throw createHttpError(401);
+  }
+
+  await Session.deleteOne({ userId: user._id });
+
+  const newSession = await createSession(user._id);
+  createCookies(res, newSession);
+
+  res.status(200).json(user);
 };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,14 @@
 import { celebrate } from 'celebrate';
 import { Router } from 'express';
-import { createUserSchema } from '../validations/authValidation.js';
-import { registerUser } from '../controllers/authController.js';
+import {
+  createUserSchema,
+  loginUserSchema,
+} from '../validations/authValidation.js';
+import { loginUser, registerUser } from '../controllers/authController.js';
 
 const router = Router();
 
 router.post('/register', celebrate(createUserSchema), registerUser);
+router.post('/login', celebrate(loginUserSchema), loginUser);
 
 export default router;
