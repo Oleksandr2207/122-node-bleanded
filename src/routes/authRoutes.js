3bed3a427e1e41c8740608e5e63658c27480aefd
Oleksandr2207 <osponomarenko09@gmail.com>
2026-04-05 15:42:18 +0300

    task-4

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,11 +4,16 @@ import {
   createUserSchema,
   loginUserSchema,
 } from '../validations/authValidation.js';
-import { loginUser, registerUser } from '../controllers/authController.js';
+import {
+  loginUser,
+  logoutController,
+  registerUser,
+} from '../controllers/authController.js';
 
 const router = Router();
 
 router.post('/register', celebrate(createUserSchema), registerUser);
 router.post('/login', celebrate(loginUserSchema), loginUser);
+router.post('/logout', logoutController);
 
 export default router;
