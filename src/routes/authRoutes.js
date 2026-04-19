93fc99a4bc2aa7a132719357bb9802506427d09a
Vasilisa-fs <vasilisa.bychkova.fs@gmail.com>
2026-04-19 15:22:38 +0300

    Task2

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,12 +3,14 @@ import { Router } from 'express';
 import {
   createUserSchema,
   loginUserSchema,
+  requestResetEmailSchema,
 } from '../validations/authValidation.js';
 import {
   loginUser,
   logoutController,
   refreshSession,
   registerUser,
+  requestResetEmail,
 } from '../controllers/authController.js';
 
 const router = Router();
@@ -17,5 +19,10 @@ router.post('/register', celebrate(createUserSchema), registerUser);
 router.post('/login', celebrate(loginUserSchema), loginUser);
 router.post('/logout', logoutController);
 router.post('/refresh', refreshSession);
+router.post(
+  '/request-reset-email',
+  celebrate(requestResetEmailSchema),
+  requestResetEmail,
+);
 
 export default router;
